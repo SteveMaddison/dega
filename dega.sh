226d0d6f012f019bb62cae21f4f8671dd772bf29
Steve Maddison <steve@cosam.org>
2010-07-01 20:51:37 +0000

    Pandora startup script changes for double buffering.

--- dega.sh
+++ dega.sh
@@ -20,13 +20,33 @@ ROM=`zenity --file-selection --title="Select a Master System/Game Gear ROM"`
 cd $PNDDIR
 dirname "$ROM" > $SAVEDIR
 
+# Get video settings
+STRETCH=0
+if [ -f videorc ] ; then
+	. videorc
+fi
+
 # Identify the ROM and set frame buffer stuff to scale accordingly.
 if [ `./dega -i "$ROM"` = "GG" ]; then
-	ofbset -fb $FBDEV -pos 160 24 -size 480 432 -mem 414720 -en 1
-	fbset -fb $FBDEV -g 160 144 160 144 16
+	case "$STRETCH" in
+		1)	# Stretch, but maintain aspect ratio
+			ofbset -fb $FBDEV -pos 133 0 -size 534 480 -mem 92160 -en 1 ;;
+		2)	# Fill entire screen
+			ofbset -fb $FBDEV -pos 0 0 -size 800 480 -mem 92160 -en 1 ;;
+		*)	# Standard integer scaling
+			ofbset -fb $FBDEV -pos 160 24 -size 480 432 -mem 92160 -en 1 ;;
+	esac
+	fbset -fb $FBDEV -g 160 144 160 288 16
 else
-	ofbset -fb $FBDEV -pos 144 48 -size 512 384 -mem 786432 -en 1
-	fbset -fb $FBDEV -g 256 192 256 192 16
+	case "$STRETCH" in
+		1)	# Stretch, but maintain aspect ratio
+			ofbset -fb $FBDEV -pos 80 0 -size 640 480 -mem 196608 -en 1 ;;
+		2)	# Fill entire screen
+			ofbset -fb $FBDEV -pos 0 0 -size 800 480 -mem 196608 -en 1 ;;
+		*)	# Standard integer scaling
+			ofbset -fb $FBDEV -pos 144 48 -size 512 384 -mem 196608 -en 1 ;;
+	esac
+	fbset -fb $FBDEV -g 256 192 256 384 16
 fi
 
 # Run the emulator
